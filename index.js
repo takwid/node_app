bccde438ac06e77210d8d6e9bcd0ba078bd7add8
takwid <sakoville@gmail.com>
2023-02-09 02:35:37 +0000

    changed port to 3000

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
-return res.send("Hello World, Desmond here! LET' GOOO!!");
+return res.send("Hello World, Desmond here! Live from Github server!");
 });
 
-// listen to port 7000 by default
-app.listen(process.env.PORT || 7000, () => {
+// listen to port 3000 by default
+app.listen(process.env.PORT || 3000, () => {
 console.log("Server is running");
 });
 
